6729f644441a1a718a48b689760bf0276966c686
Saúl Vargas <saul.vargas@uam.es>
2014-10-24 20:22:26 +0200

    MetricExample now includes more metrics

--- RankSys-tools/src/main/java/es/uam/eps/ir/ranksys/tools/examples/MetricExample.java
+++ RankSys-tools/src/main/java/es/uam/eps/ir/ranksys/tools/examples/MetricExample.java
@@ -16,19 +16,38 @@
  */
 package es.uam.eps.ir.ranksys.tools.examples;
 
+import es.uam.eps.ir.ranksys.core.data.ConcatRecommenderData;
+import es.uam.eps.ir.ranksys.core.data.RecommenderData;
+import es.uam.eps.ir.ranksys.core.data.SimpleRecommenderData;
 import es.uam.eps.ir.ranksys.core.feature.FeatureData;
 import es.uam.eps.ir.ranksys.core.feature.SimpleFeatureData;
 import es.uam.eps.ir.ranksys.core.format.RecommendationFormat;
 import es.uam.eps.ir.ranksys.core.format.SimpleRecommendationFormat;
+import es.uam.eps.ir.ranksys.core.util.parsing.Parser;
 import es.uam.eps.ir.ranksys.core.util.parsing.Parsers;
+import es.uam.eps.ir.ranksys.diversity.aggregate.metrics.AggregateDiversityMetric;
+import es.uam.eps.ir.ranksys.diversity.aggregate.metrics.GiniIndex;
+import es.uam.eps.ir.ranksys.diversity.intentaware.IntentModel;
+import es.uam.eps.ir.ranksys.diversity.intentaware.metrics.AlphaNDCG;
+import es.uam.eps.ir.ranksys.diversity.intentaware.metrics.ERRIA;
+import es.uam.eps.ir.ranksys.diversity.itemnovelty.FDItemNovelty;
+import es.uam.eps.ir.ranksys.diversity.itemnovelty.PCItemNovelty;
+import es.uam.eps.ir.ranksys.diversity.itemnovelty.PDItemNovelty;
+import es.uam.eps.ir.ranksys.diversity.itemnovelty.metrics.EFD;
+import es.uam.eps.ir.ranksys.diversity.itemnovelty.metrics.EPC;
+import es.uam.eps.ir.ranksys.diversity.itemnovelty.metrics.EPD;
+import es.uam.eps.ir.ranksys.diversity.pairwise.CosineFeatureItemDistanceModel;
 import es.uam.eps.ir.ranksys.diversity.pairwise.ItemDistanceModel;
-import es.uam.eps.ir.ranksys.diversity.pairwise.JaccardFeatureItemDistanceModel;
 import es.uam.eps.ir.ranksys.diversity.pairwise.metrics.EILD;
 import es.uam.eps.ir.ranksys.metrics.AverageRecommendationMetric;
+import es.uam.eps.ir.ranksys.metrics.NDCG;
+import es.uam.eps.ir.ranksys.metrics.Precision;
 import es.uam.eps.ir.ranksys.metrics.RecommendationMetric;
 import es.uam.eps.ir.ranksys.metrics.SystemMetric;
+import es.uam.eps.ir.ranksys.metrics.rel.BinaryRelevanceModel;
 import es.uam.eps.ir.ranksys.metrics.rel.NoRelevanceModel;
-import es.uam.eps.ir.ranksys.metrics.rel.RelevanceModel;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  *
@@ -38,20 +57,69 @@ import es.uam.eps.ir.ranksys.metrics.rel.RelevanceModel;
 public class MetricExample {
 
     public static void main(String[] args) throws Exception {
-        String featurePath = args[0];
-        int numUsers = Integer.parseInt(args[1]);
-        String recIn = args[2];
+        String trainDataPath = args[0];
+        String testDataPath = args[1];
+        String featurePath = args[2];
+        String recIn = args[3];
+        Double threshold = Double.parseDouble(args[4]);
 
-        int cutoff = 20;
+        // USER - ITEM - RATING files for train and test
+        Parser<Double> parser = (token) -> Double.parseDouble(token.toString().split("\t")[0]); // discard timestamps
+        RecommenderData<Long, Long, Double> trainData = SimpleRecommenderData.load(trainDataPath, Parsers.lp, Parsers.lp, parser);
+        RecommenderData<Long, Long, Double> testData = SimpleRecommenderData.load(testDataPath, Parsers.lp, Parsers.lp, parser);
+        RecommenderData<Long, Long, Double> totalData = new ConcatRecommenderData<>(trainData, testData);
+        // EVALUATED AT CUTOFF 10
+        int cutoff = 10;
+        // ITEM - FEATURE file
         FeatureData<Long, String, Double> featureData = SimpleFeatureData.load(featurePath, Parsers.lp, Parsers.sp, v -> 1.0);
-        ItemDistanceModel<Long> dist = new JaccardFeatureItemDistanceModel<>(featureData);
-        RelevanceModel<Long, Long> rel = new NoRelevanceModel<>();
-        RecommendationMetric<Long, Long> metric = new EILD<>(cutoff, dist, rel);
-        SystemMetric<Long, Long> average = new AverageRecommendationMetric<>(metric, numUsers);
+        // COSINE DISTANCE
+        ItemDistanceModel<Long> dist = new CosineFeatureItemDistanceModel<>(featureData);
+        // BINARY RELEVANCE
+        BinaryRelevanceModel<Long, Long> binRel = new BinaryRelevanceModel<>(false, testData, threshold);
+        // NO RELEVANCE
+        NoRelevanceModel<Long, Long> norel = new NoRelevanceModel<>();
+        // INTENT MODEL
+        IntentModel<Long, Long, String> intentModel = new IntentModel<>(false, testData.getAllUsers(), totalData, featureData);
 
+        Map<String, SystemMetric<Long, Long>> sysMetrics = new HashMap<>();
+
+        ////////////////////////
+        // INDIVIDUAL METRICS //
+        ////////////////////////
+        Map<String, RecommendationMetric<Long, Long>> recMetrics = new HashMap<>();
+
+        // PRECISION
+        recMetrics.put("prec", new Precision<>(cutoff, binRel));
+        // nDCG
+        recMetrics.put("ndcg", new NDCG<>(cutoff, new NDCG.NDCGRelevanceModel<>(false, testData, threshold)));
+        // EILD
+        recMetrics.put("eild", new EILD<>(cutoff, dist, norel));
+        // EPC
+        recMetrics.put("epc", new EPC<>(cutoff, new PCItemNovelty<>(trainData), norel));
+        // EFD
+        recMetrics.put("efd", new EFD<>(cutoff, new FDItemNovelty<>(trainData), norel));
+        // EPD
+        recMetrics.put("epd", new EPD<>(cutoff, new PDItemNovelty<>(false, trainData, dist), norel));
+        // ERR-IA
+        recMetrics.put("err-ia", new ERRIA<>(cutoff, intentModel, new ERRIA.ERRRelevanceModel<>(false, testData, threshold)));
+        // alpha-nDCG
+        recMetrics.put("a-ndcg", new AlphaNDCG<>(cutoff, 0.5, featureData, binRel));
+
+        // AVERAGE VALUES OF RECOMMENDATION METRICS FOR ITEMS IN TEST
+        int numUsers = testData.numUsers();
+        recMetrics.forEach((name, metric) -> sysMetrics.put(name, new AverageRecommendationMetric<>(metric, numUsers)));
+
+        ////////////////////
+        // SYSTEM METRICS //
+        ////////////////////
+        sysMetrics.put("aggrdiv", new AggregateDiversityMetric<>(cutoff, norel));
+        int numItems = totalData.numItems();
+        sysMetrics.put("gini", new GiniIndex<>(cutoff, numItems));
+        
         RecommendationFormat<Long, Long> format = new SimpleRecommendationFormat<>(Parsers.lp, Parsers.lp);
 
-        format.getReader(recIn).readAll().forEach(average::add);
-        System.out.println(average.evaluate());
+        format.getReader(recIn).readAll().forEach(rec -> sysMetrics.values().forEach(metric -> metric.add(rec)));
+        
+        sysMetrics.forEach((name, metric) -> System.out.println(name + "\t" + metric.evaluate()));
     }
 }
