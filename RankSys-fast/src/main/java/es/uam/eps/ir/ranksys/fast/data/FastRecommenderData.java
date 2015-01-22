340ea29d1a6e267352611c223216938e5cf029ec
Saúl Vargas <saul.vargas@uam.es>
2015-01-22 11:09:51 +0000

    forgot the license and author info

--- RankSys-fast/src/main/java/es/uam/eps/ir/ranksys/fast/data/FastRecommenderData.java
+++ RankSys-fast/src/main/java/es/uam/eps/ir/ranksys/fast/data/FastRecommenderData.java
@@ -1,6 +1,19 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/* 
+ * Copyright (C) 2014 Information Retrieval Group at Universidad Autonoma
+ * de Madrid, http://ir.ii.uam.es
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package es.uam.eps.ir.ranksys.fast.data;
 
@@ -13,7 +26,7 @@ import java.util.stream.Stream;
 
 /**
  *
- * @author saul
+ * @author Saúl Vargas (saul.vargas@uam.es)
  */
 public interface FastRecommenderData<U, I, V> extends RecommenderData<U, I, V>, FastUserIndex<U>, FastItemIndex<I> {
 
--- RankSys-fast/src/main/java/es/uam/eps/ir/ranksys/fast/utils/IdxIndex.java
+++ RankSys-fast/src/main/java/es/uam/eps/ir/ranksys/fast/utils/IdxIndex.java
@@ -1,10 +1,21 @@
-package es.uam.eps.ir.ranksys.fast.utils;
-
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/* 
+ * Copyright (C) 2014 Information Retrieval Group at Universidad Autonoma
+ * de Madrid, http://ir.ii.uam.es
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
+package es.uam.eps.ir.ranksys.fast.utils;
 
 import static es.uam.eps.ir.ranksys.core.util.FastStringSplitter.split;
 import es.uam.eps.ir.ranksys.core.util.parsing.Parser;
@@ -26,8 +37,7 @@ import static java.util.logging.Logger.getLogger;
 
 /**
  *
- * @author
- * saul
+ * @author Saúl Vargas (saul.vargas@uam.es)
  */
 public class IdxIndex<T> {
 
