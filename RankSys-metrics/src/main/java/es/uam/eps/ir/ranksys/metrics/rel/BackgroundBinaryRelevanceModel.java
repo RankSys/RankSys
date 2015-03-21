9ac365127712f6a65ee0f2eacc25e5bea10d63eb
Saúl <saul.vargas@glasgow.ac.uk>
2015-03-21 00:00:48 +0000

    - temporal novelty reranker - sales novelty - background binary relevance

---
+++ RankSys-metrics/src/main/java/es/uam/eps/ir/ranksys/metrics/rel/BackgroundBinaryRelevanceModel.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright (C) 2015 Information Retrieval Group at Universidad Autonoma
+ * de Madrid, http://ir.ii.uam.es
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package es.uam.eps.ir.ranksys.metrics.rel;
+
+import es.uam.eps.ir.ranksys.core.preference.PreferenceData;
+import es.uam.eps.ir.ranksys.metrics.rel.RelevanceModel.UserRelevanceModel;
+import it.unimi.dsi.fastutil.objects.Object2DoubleMap;
+import it.unimi.dsi.fastutil.objects.Object2DoubleOpenHashMap;
+
+/**
+ *
+ * @author Saúl Vargas (saul.vargas@uam.es)
+ */
+public class BackgroundBinaryRelevanceModel<U, I> extends RelevanceModel<U, I> {
+
+    private final PreferenceData<U, I, ?> testData;
+    private final double threshold;
+    private final double background;
+
+    public BackgroundBinaryRelevanceModel(boolean caching, PreferenceData<U, I, ?> testData, double threshold, double background) {
+        super(caching, testData.getUsersWithPreferences());
+        this.testData = testData;
+        this.threshold = threshold;
+        this.background = background;
+    }
+
+    @Override
+    protected UserRelevanceModel<U, I> get(U user) {
+        return new UserSmo4RelevanceModel(user);
+    }
+
+    private class UserSmo4RelevanceModel implements UserRelevanceModel<U, I> {
+
+        private final Object2DoubleMap<I> gainMap;
+
+        public UserSmo4RelevanceModel(U user) {
+            this.gainMap = new Object2DoubleOpenHashMap<>();
+            this.gainMap.defaultReturnValue(background);
+
+            testData.getUserPreferences(user).forEach(iv -> {
+                if (iv.v >= threshold) {
+                    gainMap.put(iv.id, 1.0);
+                } else {
+                    gainMap.put(iv.id, 0.0);
+                }
+            });
+        }
+
+        @Override
+        public boolean isRelevant(I item) {
+            return gain(item) > 0.0;
+        }
+
+        @Override
+        public double gain(I item) {
+            return gainMap.getDouble(item);
+        }
+
+    }
+}
---
+++ RankSys-noveltydiversity/RankSys-novelty/src/main/java/es/uam/eps/ir/ranksys/novelty/sales/ISDCItemNovelty.java
@@ -0,0 +1,78 @@
+/* 
+ * Copyright (C) 2014 Information Retrieval Group at Universidad Autonoma
+ * de Madrid, http://ir.ii.uam.es
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package es.uam.eps.ir.ranksys.novelty.sales;
+
+import es.uam.eps.ir.ranksys.metrics.rank.RankingDiscountModel;
+import es.uam.eps.ir.ranksys.novdiv.itemnovelty.ItemNovelty;
+import es.uam.eps.ir.ranksys.novdiv.itemnovelty.ItemNovelty.UserItemNoveltyModel;
+import java.util.List;
+import java.util.function.Function;
+
+/**
+ *
+ * @author Saúl Vargas (saul.vargas@uam.es)
+ */
+public class ISDCItemNovelty<U, I> extends ItemNovelty<U, I> {
+
+    private final int nSystems;
+    private final Function<U, List<List<I>>> otherUserRecommendations;
+    private final RankingDiscountModel disc;
+
+    public ISDCItemNovelty(int nSystems, Function<U, List<List<I>>> otherUserRecommendations, RankingDiscountModel disc) {
+        super(false, null);
+        this.nSystems = nSystems;
+        this.otherUserRecommendations = otherUserRecommendations;
+        this.disc = disc;
+    }
+
+    @Override
+    protected UserItemNoveltyModel<U, I> get(U u) {
+        return new UserISDCItemNoveltyModel(otherUserRecommendations.apply(u));
+    }
+
+    private class UserISDCItemNoveltyModel implements UserItemNoveltyModel<U, I> {
+
+        private final List<List<I>> otherRecommendations;
+
+        public UserISDCItemNoveltyModel(List<List<I>> otherRecommendations) {
+            this.otherRecommendations = otherRecommendations;
+        }
+
+        @Override
+        public double novelty(I i) {
+            if (otherRecommendations == null) {
+                return 1.0;
+            }
+            
+            double nov = otherRecommendations.stream().mapToDouble(list -> {
+                int k = list.indexOf(i);
+
+                if (k == -1) {
+                    return 1.0;
+                } else {
+                    return 1 - disc.disc(k);
+                }
+            }).sum();
+            nov /= nSystems;
+
+            return nov;
+        }
+
+    }
+
+}
