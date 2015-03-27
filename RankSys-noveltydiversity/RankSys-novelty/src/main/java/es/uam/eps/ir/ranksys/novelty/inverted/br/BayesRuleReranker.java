00161be257f249a76b7ab8198394a50d55197f2d
Saúl <saul.vargas@glasgow.ac.uk>
2015-03-27 12:04:24 +0000

    adding license template and author for BayesRuleReranker's

--- RankSys-noveltydiversity/RankSys-novelty/src/main/java/es/uam/eps/ir/ranksys/novelty/inverted/br/BayesRuleReranker.java
+++ RankSys-noveltydiversity/RankSys-novelty/src/main/java/es/uam/eps/ir/ranksys/novelty/inverted/br/BayesRuleReranker.java
@@ -1,7 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/* 
+ * Copyright (C) 2015 Information Retrieval Group at Universidad Autonoma
+ * de Madrid, http://ir.ii.uam.es
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package es.uam.eps.ir.ranksys.novelty.inverted.br;
 
@@ -13,7 +25,7 @@ import java.util.List;
 
 /**
  *
- * @author saul
+ * @author Saúl Vargas (saul.vargas@uam.es)
  */
 public abstract class BayesRuleReranker<U, I> extends PermutationReranker<U, I> {
 
