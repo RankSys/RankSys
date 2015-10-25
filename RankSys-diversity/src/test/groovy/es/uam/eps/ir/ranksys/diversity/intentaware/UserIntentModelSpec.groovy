c14fe536872120aede45cf1c9486d33ac67c9795
Saúl <saul.vargas@glasgow.ac.uk>
2015-10-25 15:29:48 +0000

    RankSys-diversity uses MPL

--- RankSys-diversity/src/test/groovy/es/uam/eps/ir/ranksys/diversity/intentaware/UserIntentModelSpec.groovy
+++ RankSys-diversity/src/test/groovy/es/uam/eps/ir/ranksys/diversity/intentaware/UserIntentModelSpec.groovy
@@ -1,3 +1,11 @@
+/* 
+ * Copyright (C) 2015 Information Retrieval Group at Universidad Autónoma
+ * de Madrid, http://ir.ii.uam.es
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package es.uam.eps.ir.ranksys.diversity.intentaware
 
 import es.uam.eps.ir.ranksys.core.feature.FeatureData
