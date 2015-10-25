6c43df6ecc1d3ac45d05516ef2a873693bfd4935
Saúl <saul.vargas@glasgow.ac.uk>
2015-10-25 15:03:06 +0000

    RankSys-compression uses MPL

--- RankSys-compression/src/main/java/org/ranksys/compression/preferences/RatingCODECPreferenceData.java
+++ RankSys-compression/src/main/java/org/ranksys/compression/preferences/RatingCODECPreferenceData.java
@@ -1,18 +1,9 @@
 /* 
- * Copyright (C) 2015 RankSys http://ranksys.github.io
+ * Copyright (C) 2015 RankSys http://ranksys.org
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 package org.ranksys.compression.preferences;
 
@@ -31,19 +22,13 @@ import es.uam.eps.ir.ranksys.fast.preference.FastPreferenceData;
 import es.uam.eps.ir.ranksys.fast.preference.FasterPreferenceData;
 import es.uam.eps.ir.ranksys.fast.preference.TransposedPreferenceData;
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.UncheckedIOException;
-import java.util.function.BiConsumer;
 import java.util.function.Function;
-import static java.util.stream.Collectors.joining;
 import java.util.stream.IntStream;
 import static java.util.stream.IntStream.range;
 import java.util.stream.Stream;
