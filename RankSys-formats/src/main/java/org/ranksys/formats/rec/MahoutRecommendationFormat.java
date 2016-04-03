6dcdf0400cc2d8f7824eed06cc2ad4a1675664c0
Saúl <saul.vargas@glasgow.ac.uk>
2016-04-03 09:52:07 +0100

    added recommendation formats for TREC, Mahout and zipped Simple

---
+++ RankSys-formats/src/main/java/org/ranksys/formats/rec/MahoutRecommendationFormat.java
@@ -0,0 +1,145 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.ranksys.formats.rec;
+
+import es.uam.eps.ir.ranksys.core.IdDouble;
+import es.uam.eps.ir.ranksys.core.Recommendation;
+import es.uam.eps.ir.ranksys.core.util.FastStringSplitter;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.UncheckedIOException;
+import java.util.List;
+import java.util.stream.Collectors;
+import static java.util.stream.Collectors.joining;
+import java.util.stream.Stream;
+import org.ranksys.formats.parsing.Parser;
+import static org.ranksys.formats.parsing.Parsers.dp;
+
+/**
+ *
+ * @author saul
+ */
+public class MahoutRecommendationFormat<U, I> implements RecommendationFormat<U, I> {
+
+    private final Parser<U> uParser;
+    private final Parser<I> iParser;
+    private final Parser<Double> vParser = dp;
+
+    public MahoutRecommendationFormat(Parser<U> uParser, Parser<I> iParser) {
+        this.uParser = uParser;
+        this.iParser = iParser;
+    }
+
+    @Override
+    public Writer getWriter(String path) throws IOException {
+        return new Writer(path);
+    }
+
+    @Override
+    public Writer<U, I> getWriter(File file) throws IOException {
+        return getWriter(file.getPath());
+    }
+
+    @Override
+    public Writer<U, I> getWriter(OutputStream out) throws IOException {
+        throw new UnsupportedOperationException("mahout format needs to know the path, sorry!");
+    }
+
+    public static class Writer<U, I> implements RecommendationFormat.Writer<U, I> {
+
+        private final BufferedWriter writer;
+
+        public Writer(String path) throws IOException {
+            File file = new File(path);
+            file.mkdir();
+            this.writer = new BufferedWriter(new FileWriter(path + "/part-0"));
+        }
+
+        @Override
+        public void write(Recommendation<U, I> recommendation) throws IOException {
+            writer.write(recommendation.getUser() + "\t");
+            writer.write(recommendation.getItems().stream().map(is -> is.id + ":" + is.v).collect(joining(",", "[", "]")));
+            writer.newLine();
+        }
+
+        @Override
+        public void close() throws IOException {
+            writer.flush();
+            writer.close();
+        }
+
+    }
+
+    @Override
+    public Reader<U, I> getReader(String path) throws IOException {
+        return new Reader<>(uParser, iParser, vParser, path);
+    }
+
+    @Override
+    public Reader<U, I> getReader(File file) throws IOException {
+        return getReader(file.getPath());
+    }
+
+    @Override
+    public Reader<U, I> getReader(InputStream in) throws IOException {
+        throw new UnsupportedOperationException("mahout format needs to know the path, sorry!");
+    }
+
+    public static class Reader<U, I> implements RecommendationFormat.Reader<U, I> {
+
+        private final Parser<U> uParser;
+        private final Parser<I> iParser;
+        private final Parser<Double> vParser;
+        private final String path;
+
+        public Reader(Parser<U> uParser, Parser<I> iParser, Parser<Double> vParser, String path) {
+            this.uParser = uParser;
+            this.iParser = iParser;
+            this.vParser = vParser;
+            this.path = path;
+        }
+
+        @Override
+        public Stream<Recommendation<U, I>> readAll() throws IOException {
+            File[] files = new File(path).listFiles((dir, name) -> name.startsWith("part-"));
+            return Stream.of(files)
+                    .map(file -> {
+                        try {
+                            return loadPart(new BufferedReader(new FileReader(file)));
+                        } catch (IOException ex) {
+                            throw new UncheckedIOException(ex);
+                        }
+                    })
+                    .reduce(Stream.empty(), Stream::concat);
+        }
+
+        private Stream<Recommendation<U, I>> loadPart(BufferedReader reader) throws IOException {
+            return reader.lines().map(line -> {
+                CharSequence[] toks1 = FastStringSplitter.split(line, '\t');
+
+                U user = uParser.parse(toks1[0]);
+
+                CharSequence[] toks2 = FastStringSplitter.split(toks1[1].subSequence(1, toks1[1].length() - 1), ',');
+                List<IdDouble<I>> items = Stream.of(toks2).map(is -> {
+                    CharSequence[] toks3 = FastStringSplitter.split(is, ':');
+
+                    I i = iParser.parse(toks3[0]);
+                    double s = vParser.parse(toks3[1]);
+
+                    return new IdDouble<>(i, s);
+                }).collect(Collectors.toList());
+
+                return new Recommendation<U, I>(user, items);
+            });
+        }
+    }
+}
---
+++ RankSys-formats/src/main/java/org/ranksys/formats/rec/ZipRecommendationFormat.java
@@ -0,0 +1,211 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.ranksys.formats.rec;
+
+import es.uam.eps.ir.ranksys.core.IdDouble;
+import es.uam.eps.ir.ranksys.core.Recommendation;
+import static es.uam.eps.ir.ranksys.core.util.FastStringSplitter.split;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Spliterators;
+import java.util.logging.Level;
+import static java.util.logging.Logger.getLogger;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
+import org.ranksys.formats.parsing.Parser;
+import static org.ranksys.formats.parsing.Parsers.pdp;
+
+/**
+ *
+ * @author saul
+ */
+public class ZipRecommendationFormat<U, I> implements RecommendationFormat<U, I> {
+
+    private final Parser<U> uParser;
+    private final Parser<I> iParser;
+    private final boolean ignoreScores;
+
+    public ZipRecommendationFormat(Parser<U> uParser, Parser<I> iParser, boolean ignoreScores) {
+        this.uParser = uParser;
+        this.iParser = iParser;
+        this.ignoreScores = ignoreScores;
+    }
+
+    @Override
+    public Writer getWriter(OutputStream out) throws IOException {
+        return new Writer(out, ignoreScores);
+    }
+
+    public static class Writer<U, I> implements RecommendationFormat.Writer<U, I> {
+
+        private final ZipOutputStream zip;
+        private final BufferedWriter writer;
+        private final boolean ignoreScores;
+
+        public Writer(OutputStream out, boolean ignoreScores) throws FileNotFoundException, IOException {
+            this.zip = new ZipOutputStream(out);
+            zip.putNextEntry(new ZipEntry("recommendation"));
+            writer = new BufferedWriter(new OutputStreamWriter(zip));
+            this.ignoreScores = ignoreScores;
+        }
+
+        @Override
+        public void write(Recommendation<U, I> recommendation) throws IOException {
+
+            U u = recommendation.getUser();
+            for (IdDouble<I> pair : recommendation.getItems()) {
+                writer.write(u.toString());
+                writer.write("\t");
+                writer.write(pair.id.toString());
+                if (!ignoreScores) {
+                    writer.write("\t");
+                    writer.write(Double.toString(pair.v));
+                }
+                writer.newLine();
+            }
+        }
+
+        @Override
+        public void close() throws IOException {
+            writer.flush();
+            zip.closeEntry();
+            zip.close();
+        }
+
+    }
+
+    @Override
+    public Reader<U, I> getReader(InputStream in) throws IOException {
+        return new Reader<>(uParser, iParser, ignoreScores, in);
+    }
+
+    public static class Reader<U, I> implements RecommendationFormat.Reader<U, I> {
+
+        private final Parser<U> uParser;
+        private final Parser<I> iParser;
+        private final boolean ignoreScores;
+        private final InputStream in;
+
+        public Reader(Parser<U> uParser, Parser<I> iParser, boolean ignoreScores, InputStream in) {
+            this.uParser = uParser;
+            this.iParser = iParser;
+            this.ignoreScores = ignoreScores;
+            this.in = in;
+        }
+
+        @Override
+        public Stream<Recommendation<U, I>> readAll() throws IOException {
+            try {
+                ZipInputStream zip = new ZipInputStream(in);
+                zip.getNextEntry();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(zip), 128 * 1024);
+
+                RecommendationIterator iterator = new RecommendationIterator(reader, uParser, iParser, ignoreScores);
+                return StreamSupport.stream(Spliterators.spliteratorUnknownSize(iterator, 0), false);
+            } catch (IOException ex) {
+                getLogger(ZipRecommendationFormat.class.getName()).log(Level.SEVERE, null, ex);
+                return null;
+            }
+        }
+
+    }
+
+    private static class RecommendationIterator<U, I> implements Iterator<Recommendation<U, I>> {
+
+        private U lastU = null;
+        private I lastI;
+        private double lastS;
+        private final BufferedReader reader;
+        private final Parser<U> uParser;
+        private final Parser<I> iParser;
+        private final boolean ignoreScores;
+        private boolean eos = false;
+
+        public RecommendationIterator(BufferedReader reader, final Parser<U> uParser, final Parser<I> iParser, final boolean ignoreScores) throws IOException {
+            this.reader = reader;
+            this.uParser = uParser;
+            this.iParser = iParser;
+            this.ignoreScores = ignoreScores;
+        }
+
+        @Override
+        public boolean hasNext() {
+            if (eos) {
+                return false;
+            }
+            if (lastU == null) {
+                String line = null;
+                try {
+                    line = reader.readLine();
+                } catch (IOException ex) {
+                    getLogger(Recommendation.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                if (line == null) {
+                    try {
+                        reader.close();
+                    } catch (IOException ex) {
+                        getLogger(Recommendation.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                    return false;
+                } else {
+                    CharSequence[] tokens = split(line, '\t', ignoreScores ? 3 : 4);
+                    lastU = uParser.parse(tokens[0]);
+                    lastI = iParser.parse(tokens[1]);
+                    lastS = ignoreScores ? 1.0 : pdp.applyAsDouble(tokens[2]);
+                    return true;
+                }
+            } else {
+                return true;
+            }
+        }
+
+        @Override
+        public Recommendation<U, I> next() {
+            String line = null;
+
+            List<IdDouble<I>> list = new ArrayList<>();
+
+            U nextU = lastU;
+            list.add(new IdDouble<>(lastI, lastS));
+            try {
+                while ((line = reader.readLine()) != null) {
+                    CharSequence[] tokens = split(line, '\t', ignoreScores ? 3 : 4);
+                    U u = uParser.parse(tokens[0]);
+                    I i = iParser.parse(tokens[1]);
+                    double s = ignoreScores ? 1.0 : pdp.applyAsDouble(tokens[2]);
+                    if (u.equals(lastU)) {
+                        list.add(new IdDouble<>(i, s));
+                    } else {
+                        lastU = u;
+                        lastI = i;
+                        lastS = s;
+                        break;
+                    }
+                }
+            } catch (IOException ex) {
+                getLogger(Recommendation.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            if (line == null) {
+                lastU = null;
+                eos = true;
+            }
+
+            return new Recommendation<>(nextU, list);
+        }
+    }
+}
