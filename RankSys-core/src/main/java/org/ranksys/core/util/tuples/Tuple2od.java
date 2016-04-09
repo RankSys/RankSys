9b5ddba83fbcfad58abd0879211d15c73e47cda0
Saúl <saul.vargas@glasgow.ac.uk>
2016-04-09 15:28:41 +0100

    added javadoc to RankSys-core

--- RankSys-core/src/main/java/org/ranksys/core/util/tuples/Tuple2od.java
+++ RankSys-core/src/main/java/org/ranksys/core/util/tuples/Tuple2od.java
@@ -13,35 +13,75 @@ import org.jooq.lambda.tuple.Tuple;
 import org.jooq.lambda.tuple.Tuple2;
 
 /**
+ * Tuple of object-double.
  *
  * @author Saúl Vargas (Saul@VargasSandoval.es)
+ * @param <T1> type of object.
  */
 public class Tuple2od<T1> implements Comparable<Tuple2od<T1>>, Serializable, Cloneable {
 
+    /**
+     * First value (object).
+     */
     public final T1 v1;
+
+    /**
+     * Second value (double).
+     */
     public final double v2;
 
+    /**
+     * Constructor from a jOOL tuple.
+     *
+     * @param tuple tuple to be copied.
+     */
     public Tuple2od(Tuple2<T1, Double> tuple) {
         this(tuple.v1, tuple.v2);
     }
 
+    /**
+     * Constructor from an object-double tuple.
+     *
+     * @param tuple tuple to be copied
+     */
     public Tuple2od(Tuple2od<T1> tuple) {
         this(tuple.v1, tuple.v2);
     }
 
+    /**
+     * Constructor from separate object and double values.
+     *
+     * @param v1 object value
+     * @param v2 double value
+     */
     public Tuple2od(T1 v1, double v2) {
         this.v1 = v1;
         this.v2 = v2;
     }
 
+    /**
+     * Returns the first element (object).
+     *
+     * @return first element (object).
+     */
     public T1 v1() {
         return v1;
     }
 
+    /**
+     * Returns the second element (double).
+     *
+     * @return second element (double).
+     */
     public double v2() {
         return v2;
     }
 
+    /**
+     * Converts the tuple into a jOOL tuple.
+     *
+     * @return jOOL tuple
+     */
     public Tuple2<T1, Double> asTuple() {
         return Tuple.tuple(v1, v2);
     }
--- RankSys-core/src/main/java/org/ranksys/core/util/tuples/Tuples.java
+++ RankSys-core/src/main/java/org/ranksys/core/util/tuples/Tuples.java
@@ -8,19 +8,43 @@
 package org.ranksys.core.util.tuples;
 
 /**
+ * Convenient static builders of tuples.
  *
  * @author Saúl Vargas (Saul@VargasSandoval.es)
  */
 public class Tuples {
 
+    /**
+     * Creates a tuple of object-double.
+     *
+     * @param <T1> type of object
+     * @param v1 object value
+     * @param v2 double value
+     * @return tuple
+     */
     public static <T1> Tuple2od<T1> tuple(T1 v1, double v2) {
         return new Tuple2od<>(v1, v2);
     }
 
+    /**
+     * Creates a tuple of integer-object.
+     *
+     * @param <T2> type of object
+     * @param v1 integer value
+     * @param v2 object value
+     * @return tuple
+     */
     public static <T2> Tuple2io<T2> tuple(int v1, T2 v2) {
         return new Tuple2io<>(v1, v2);
     }
 
+    /**
+     * Creates a tuple of integer-double.
+     *
+     * @param v1 integer value
+     * @param v2 double value
+     * @return tuple
+     */
     public static Tuple2id tuple(int v1, double v2) {
         return new Tuple2id(v1, v2);
     }
