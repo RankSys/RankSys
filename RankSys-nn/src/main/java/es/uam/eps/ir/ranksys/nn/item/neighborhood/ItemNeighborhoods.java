59c782c2d394120b668ecc0b0d5ef8d7452c6ab1
Saúl <saul.vargas@glasgow.ac.uk>
2016-09-09 01:03:36 +0100

    diaomond operator

--- RankSys-nn/src/main/java/es/uam/eps/ir/ranksys/nn/item/neighborhood/ItemNeighborhoods.java
+++ RankSys-nn/src/main/java/es/uam/eps/ir/ranksys/nn/item/neighborhood/ItemNeighborhoods.java
@@ -15,19 +15,19 @@ import static org.ranksys.core.util.tuples.Tuples.tuple;
 public class ItemNeighborhoods {
 
     public static <I> ItemNeighborhood<I> topK(ItemSimilarity<I> similarity, int k) {
-        return new ItemNeighborhood<I>(similarity, new TopKNeighborhood(similarity.similarity(), k));
+        return new ItemNeighborhood<>(similarity, new TopKNeighborhood(similarity.similarity(), k));
     }
 
     public static <I> ItemNeighborhood<I> threshold(ItemSimilarity<I> similarity, double threshold) {
-        return new ItemNeighborhood<I>(similarity, new ThresholdNeighborhood(similarity.similarity(), threshold));
+        return new ItemNeighborhood<>(similarity, new ThresholdNeighborhood(similarity.similarity(), threshold));
     }
 
     public static <I> ItemNeighborhood<I> cached(ItemNeighborhood<I> neighborhood) {
-        return new ItemNeighborhood<I>(neighborhood, new CachedNeighborhood(neighborhood.numItems(), neighborhood.neighborhood()));
+        return new ItemNeighborhood<>(neighborhood, new CachedNeighborhood(neighborhood.numItems(), neighborhood.neighborhood()));
     }
 
     public static <I> ItemNeighborhood<I> cached(FastItemIndex<I> items, Stream<Tuple2<I, Stream<Tuple2od<I>>>> neighborhoods) {
-        return new ItemNeighborhood<I>(items, new CachedNeighborhood(items.numItems(), neighborhoods
+        return new ItemNeighborhood<>(items, new CachedNeighborhood(items.numItems(), neighborhoods
                 .map(t -> tuple(items.item2iidx(t.v1), t.v2.map(items::item2iidx)))));
     }
 
